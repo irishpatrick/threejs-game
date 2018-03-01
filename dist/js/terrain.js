c5169d849835f687ab06484970f36b5604a4bedf
Patrick Roche <rochep63@gmail.com>
2018-02-28 22:08:47 -0600

    lf endings and fixed terrain file loading

--- dist/js/terrain.js
+++ dist/js/terrain.js
@@ -2,10 +2,10 @@ let fs = require("fs");
 let path = require("path");
 let THREE = require("three");
 
-let assetpath = path.join(__dirname, "dist", "assets");
+let assetpath = path.join(__dirname, "..", "assets");
 
 function normalize(a) {
-    
+
 }
 
 class Terrain {
@@ -14,13 +14,12 @@ class Terrain {
         let mat = new THREE.MeshToonMaterial({color: 0x00dd00, side: THREE.DoubleSide});
         this.mesh = new THREE.Mesh(geo, mat);
         this.mesh.rotation.x = Math.PI / 2;
-        this.json = {};
 
+        let ref = this;
         fs.readFile(path.join(assetpath, fn), function(err, data) {
             if (!err) {
-                console.log("received data: " + data);
-                this.json = JSON.parse(data);
-                this.process();
+                console.log(JSON.parse(data));
+                ref.process(JSON.parse(data));
 
             } else {
                 console.log(err);
@@ -32,8 +31,10 @@ class Terrain {
         return this.mesh;
     }
 
-    process() {
-        for (let feature in this.json.features) {
+    process(json) {
+        console.log(json);
+        for (let feature of json.features) {
+            console.log(feature);
             for (let vector in this.mesh.geometry.vertices) {
                 let center = new THREE.Vector3(feature.location[0], feature.location[1], 0);
                 let dist = center.distanceTo(vector);
